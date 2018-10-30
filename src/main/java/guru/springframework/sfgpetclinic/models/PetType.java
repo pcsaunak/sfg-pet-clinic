c772b58ed4b8d68df85b36a7fa624cb9103b35f2
saunaklogan <saunak.choudhury@icrushiflush.com>
2018-10-30 06:36:44 +0530

    Adding pojos to Git. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/models/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.models;
+
+public class PetType {
+    private String petName;
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+}
