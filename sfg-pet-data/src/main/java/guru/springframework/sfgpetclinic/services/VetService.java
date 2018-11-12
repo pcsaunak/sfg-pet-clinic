fe52b20c3d4fd197dc4983ffefbbbe0428945a44
saunaklogan <saunak.choudhury@icrushiflush.com>
2018-11-12 18:44:06 +0530

    Creating common base interface and cleaning up duplicate functions . Closes #11

--- sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -3,10 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.models.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
 }
