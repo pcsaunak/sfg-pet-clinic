87ed706ba28b6d39bfd02b2812efc99fb7b70d75
saunaklogan <saunak.choudhury@icrushiflush.com>
2018-11-24 11:52:33 +0530

    Added logic for generating ID automtically . closes #23

--- sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.models.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>(); // adding and removing properties
+import java.util.*;
+
+
+/*
+* entity type must extends Base Entity
+* ID type must extend Long
+* */
+
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>(); // adding and removing properties
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,8 +22,16 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id,T object){
-        map.put(id,object);
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -27,6 +41,17 @@ public abstract class AbstractMapService<T,ID> {
 
     void delete (T object){
         // An interface should contain only one unimplemented function for that to work.
+        // In that case we can have lambda expression.
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch (NoSuchElementException e){
+            nextId=1l;
+        }
+        return nextId + 1;
     }
 }
--- sfg-pet-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -33,14 +33,12 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
-        owner1.setId(1l);
 
         ownerService.save(owner1);
 
         System.out.println("Saved Owner 1");
 
         Owner owner2 = new Owner();
-        owner2.setId(2l);
         owner2.setFirstName("Shemrock");
         owner2.setLastName("Fine");
 
@@ -48,7 +46,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Saved Owner 2");
 
         Vet vet1 = new Vet();
-        vet1.setId(1l);
+
         vet1.setFirstName("Roman");
         vet1.setLastName("Nermar");
 
@@ -57,7 +55,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Saved vet - 1");
 
         Vet vet2 = new Vet();
-        vet2.setId(2l);
         vet2.setFirstName("Suram");
         vet2.setLastName("Zoram");
 
