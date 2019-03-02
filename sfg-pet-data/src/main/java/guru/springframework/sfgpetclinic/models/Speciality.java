11dd2e766a479a77464c406ba69fbb4e91152b01
saunaklogan <saunak.choudhury@icrushiflush.com>
2019-03-02 09:08:41 +0530

    Updated Vets pojo converted to JPA. Closes#37

--- sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/models/Speciality.java
+++ sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/models/Speciality.java
@@ -1,6 +1,14 @@
 package guru.springframework.sfgpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//This is mapped to DB
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
+++ sfg-pet-data/src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    //JPA would try to load all the relationship all at once.
+    // Default is set to Lazy which will not load properties until it is asked for.
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
